8b13e74fe4701a26d3adda7da744623084794fd6
Tiago Chedraoui Silva <tiagochst@gmail.com>
2021-04-30 14:22:06 -0400

    #2 reduce OrderCpGsByLocation example

--- R/util1_OrderCpGsByLocation.R
+++ R/util1_OrderCpGsByLocation.R
@@ -20,6 +20,7 @@
 #'      output = "dataframe"
 #'    )
 #'
+#' \dontrun{
 #'    CpGs_char <- c("cg04677227", "cg07146435", "cg11632906", "cg20214853")
 #'    OrderCpGsByLocation(
 #'      CpGs_char,
@@ -27,7 +28,7 @@
 #'      arrayType = "450k",
 #'      output = "vector"
 #'    )
-#'
+#' }
 OrderCpGsByLocation <- function(
   CpGs_char,
   genome = c("hg19","hg38"),
