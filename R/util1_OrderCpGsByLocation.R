19d4566d13c3c0df12c4cd359abc7aef35089b27
Lijiao Ning <lijiao.ning@cnrs.fr>
2022-01-03 16:58:36 +0100

    ignore strand while sorting
    To avoid end position bigger than start position in annotation in case of co-methylated CpGs are not in the same strand and the strand information is not important.

--- R/util1_OrderCpGsByLocation.R
+++ R/util1_OrderCpGsByLocation.R
@@ -1,9 +1,10 @@
 #' Order CpGs by genomic location
 #'
 #' @param CpGs_char vector of CpGs
-#' @param arrayType Type of array: 450k or EPIC
-#' @param output vector of CpGs or dataframe with CpGs, CHR, MAPINFO
 #' @param genome Human genome of reference: hg19 or hg38
+#' @param arrayType Type of array: 450k or EPIC
+#' @param ignoreStrand Whether strand can be ignored, default is TRUE
+#' @param output vector of CpGs or dataframe with CpGs, CHR, MAPINFO
 #'
 #' @return vector of CpGs ordered by location or dataframe with CpGs ordered by
 #'   location (cpg), chromosome (chr), position (pos)
@@ -19,6 +20,7 @@
 #'    CpGs_char,
 #'    genome = "hg19",
 #'    arrayType = "450k",
+#'    ignoreStrand = TRUE,
 #'    output = "dataframe"
 #'  )
 #' 
@@ -26,6 +28,7 @@ OrderCpGsByLocation <- function(
   CpGs_char,
   genome = c("hg19", "hg38"),
   arrayType = c("450k", "EPIC"),
+  ignoreStrand = TRUE,
   output = c("vector", "dataframe")
 ){
 
@@ -52,7 +55,7 @@ OrderCpGsByLocation <- function(
     )
   }
   CpGs.gr <- sort(
-    CpGlocations.gr[ CpGs_char[goodCpGs_lgl] ]
+    CpGlocations.gr[CpGs_char[goodCpGs_lgl], ignore.strand = ignoreStrand]
   )
 
   ### Select and return output ###
